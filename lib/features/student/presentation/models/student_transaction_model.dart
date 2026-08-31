06311ba89382026852682cf3e09860ed56be221f
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-08-31 23:31:48 +0300

    Student models definition

---
+++ lib/features/student/presentation/models/student_transaction_model.dart
@@ -0,0 +1,13 @@
+class StudentTxnItem {
+  final String merchant;
+  final double amount;
+  final String time;
+  final String cat;
+
+  const StudentTxnItem({
+    required this.merchant,
+    required this.amount,
+    required this.time,
+    required this.cat,
+  });
+}
