05309a2ffc0cd26fd6384459498169492d175335
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-09-04 15:55:33 +0300

    Implementation of the package info package

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class Spacing extends SizedBox {
   const Spacing.vertical(double height, {super.key}) : super(height: height);
 
   const Spacing.horizontal(double width, {super.key}) : super(width: width);
 }
+
+late PackageInfo packageInfo;
